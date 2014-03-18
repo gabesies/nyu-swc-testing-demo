39a9aa2071185275ef2b10e63850532f916c45c8
Gabe Perez-Giz <gabe.perezgiz@nyu.edu>
2014-03-18 14:35:33 -0400

    Basic outline and boilerplate for nosetest exercise

---
+++ coordinates2region.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+"""This is the module coordinates2region.py"""
+
+def coordinates2region():
+    """Blah"""
+    pass
+
+
+def main():
+    """Bar"""
+    print "Hi!"
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
---
+++ test_coordinates2region.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+"""This is the nosetest module for the coordinate2region.py"""
+
+def test_foo():
+    """Bar"""
+    pass
+
+
